27d543d548194ea7eeda965b1a625c38f6a6c5d1
Michael Böhme <boehme.mic@gmail.com>
2020-06-26 20:17:20 +0200

    added statistics for the transmission risk levels (TRLs)

--- dka_downloader.py
+++ dka_downloader.py
@@ -53,20 +53,46 @@ def anonymize_TEKs(filename):
             f.write(tek_pattern.sub("XXXXXXXXXXXXXXXXXXXXXXXX,", line) + "\n")
         f.close()       
 
+
+def process_trl_data(filename):
+    
+    data = [0, 0, 0, 0, 0, 0, 0, 0]
+    trl_pattern = re.compile(r"Transmission Risk Level: ([0-8])")
+    
+    with open(filename, 'r') as f:
+        raw_data = f.read().splitlines()
+        f.close()
+        
+    for line in raw_data:
+        pt = trl_pattern.search(line)
+        if pt is not None:
+            data[int(pt.group(1))-1] += 1
+    
+    return data
+
     
 if __name__ == "__main__":
-    DATA_DIR  = os.path.dirname(os.path.realpath(__file__)) + "/daily_data/"
-    CSV_FILE  = DATA_DIR + "diagnosis_keys_statistics.csv"
-    JSON_FILE = DATA_DIR + "diagnosis_keys_statistics.json"
     
-    # download daily data list    
+    ###########################################################################
+    ##### Paths and filenames
+    ###########################################################################
+    DATA_DIR  = os.path.dirname(os.path.realpath(__file__)) + "/daily_data/"
+    
+    DKS_CSV_FILE  = DATA_DIR + "diagnosis_keys_statistics.csv"
+    DKS_JSON_FILE = DATA_DIR + "diagnosis_keys_statistics.json"
+    
+    TRL_CSV_FILE  = DATA_DIR + "transmission_risk_level_statistics.csv"
+    TRL_JSON_FILE = DATA_DIR + "transmission_risk_level_statistics.json"
+    
+    # download list with daily data sets
     daily_list = getDailyDownloadList(DATA_DIR)
     
+    # stop immediately if download failed
     if ( daily_list == False ):
         print("Error! No daily list with diagnosis keys found!")
         sys.exit(1)
         
-    # download daily files
+    # download daily data set files
     for daily_report in daily_list:
         filename = DATA_DIR + daily_report + ".zip"
         if not os.path.isfile(filename):
@@ -83,8 +109,9 @@ if __name__ == "__main__":
     pattern_num_users = re.compile(r"([0-9]{1,}) user\(s\) found\.")
     pattern_num_subm = re.compile(r"([0-9]{1,}) user\(s\): ([0-9]{1,}) Diagnosis Key\(s\)")
     date_list = []
+    data_files = []
     
-    # read files
+    # read processed files
     for r, d, f in os.walk(DATA_DIR):
         for file in f:
             if file.endswith(".dat"):
@@ -96,6 +123,7 @@ if __name__ == "__main__":
                         raw_data = df.read()
                     
                     timestamp = int(datetime.datetime(year=int(pd[0][0]), month=int(pd[0][1]), day=int(pd[0][2]), hour=2).strftime("%s"))
+                    data_files.append([file, timestamp])
     
                     # number of diagnosis keys                
                     num_keys = 0
@@ -119,6 +147,10 @@ if __name__ == "__main__":
                         
                     date_list.append([timestamp, num_keys, num_users, num_subbmited_keys, 0, 0, 0])
     
+    ###########################################################################
+    ##### Temporary Exposure Keys (TEKs)
+    ###########################################################################
+    
     # add an empty entry as origin (2020-06-22)
     date_list.append([1592784000, 0, 0, 0, 0, 0, 0])
     
@@ -138,11 +170,48 @@ if __name__ == "__main__":
         str_csv += "{},{},{},{},{},{},{}\n".format(line[0], line[1], line[2], line[3], line[4], line[5], line[6])
         
     # write csv to disk
-    with open(CSV_FILE, 'w') as f:
+    with open(DKS_CSV_FILE, 'w') as f:
         f.write(str_csv)
         f.close()
     
     # write json to disk
-    with open(JSON_FILE, 'w') as f:
+    with open(DKS_JSON_FILE, 'w') as f:
         f.write(json.dumps(sorted_data_list, sort_keys=True))
         f.close()
+    
+    ###########################################################################
+    ##### Transmission Risk Levels (TRLs)
+    ###########################################################################
+        
+    # add an empty entry as origin (2020-06-22)
+    trl_data = [ [1592784000, [0, 0, 0, 0, 0, 0, 0, 0]] ]
+    sum_data = [ 0, 0, 0, 0, 0, 0, 0, 0 ]
+    
+    # process transmission risk level data
+    for filename, timestamp in data_files:
+        new_data = [timestamp, process_trl_data(DATA_DIR + filename)]
+        trl_data.append(new_data)
+        for i, val in enumerate(new_data[1]):
+            sum_data[i] += val
+            
+    # sort list by timestamp
+    sorted_trl_data = sorted(trl_data, key=lambda t: t[0])
+    
+    # add last line with sums
+    sorted_trl_data.append([0, sum_data])
+            
+    # generate csv
+    str_trl_csv = "#timestamp, num_TRL_1, num_TRL_2, num_TRL_3, num_TRL_4, num_TRL_5, num_TRL_6, num_TRL_7, num_TRL_8\n"
+    for timestamp, data in sorted_trl_data:
+        str_trl_csv += "{:10d},{},{},{},{},{},{},{},{}\n".format(timestamp, data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7])
+    
+    # write csv to disk
+    with open(TRL_CSV_FILE, 'w') as f:
+        f.write(str_trl_csv)
+        f.close()
+        
+    # write json to disk
+    with open(TRL_JSON_FILE, 'w') as f:
+        f.write(json.dumps(sorted_trl_data, sort_keys=True))
+        f.close()
+    
