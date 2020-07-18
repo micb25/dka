c3cb214afeae8911b8e9e1d8a63db98c9089594b
Michael Böhme <boehme.mic@gmail.com>
2020-07-18 03:15:04 +0200

    added first implementation of a parsing script for the CWA config

---
+++ data_CWA/app_config_scripts/cwa_parse_config.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import CWAConfig_pb2 as CWAConfig
+
+config = CWAConfig.ApplicationConfiguration()
+with open("../app_config/app_config_2020-07-18.bin", "rb") as fd:
+    config.ParseFromString(fd.read())
+
+print(config) 
