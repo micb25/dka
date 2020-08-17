35c49df9831badb944d74676e2884a9bdda42cc3
Michael Böhme <boehme.mic@gmail.com>
2020-08-18 01:00:56 +0200

    added protobuf dump of the current app config (#6)

--- data_CWA/app_config_scripts/cwa_parse_config.py
+++ data_CWA/app_config_scripts/cwa_parse_config.py
@@ -4,7 +4,7 @@
 import CWAConfig_pb2 as CWAConfig
 
 config = CWAConfig.ApplicationConfiguration()
-with open("../app_config/app_config_2020-07-18.bin", "rb") as fd:
+with open("../app_config/app_config_2020-08-17.bin", "rb") as fd:
     config.ParseFromString(fd.read())
 
 print(config) 
